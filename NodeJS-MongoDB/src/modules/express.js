a9a4295f350aa9b634ebf00d5212ff6d30908c1b
toni500madmax <toni500madmax@gmail.com>
2025-04-15 21:01:27 -0300

    feat: completando projeto backend métodos Get, Post, Patch e Delete

--- NodeJS-MongoDB/src/modules/express.js
+++ NodeJS-MongoDB/src/modules/express.js
@@ -1,34 +1,34 @@
+// Importando Express
 const express = require("express");
-
+// Definindo a porta
 const port = 8080;
-
+// Importando o modelo de usuário
 const UserModel = require("../models/user.model");
-
+// Criando uma aplicação da função do Express
 const app = express();
-
+// Definindo que todo documento enviado será por padrão JSON
 app.use(express.json());
 
-app.get("/home", (req, res) => {
-    res.contentType("application/html");
-    res.status(200).send("<h1>Hello World, this is Home</h1>");
+app.use((req, res, next) => {
+    console.log(`Request Type: ${req.method}`);
+    console.log(`Content Type: ${req.headers["content-type"]}`);
+    console.log(`Body Object: ${JSON.stringify(req.body)}`);
+    console.log(`Date: ${new Date()}`)
+    next();
 });
 
-app.get("/users", (req, res) => {
-    const users = [
-        {
-            name: "Hester Morales",
-            email: "hesles@email.com"
-        },
-        {
-            name: "Lelia Drake",
-            email: "lelisKe@email.com"
-        }
-    ];
-
-    res.status(200).json(users);
+// Método Get de Users
+app.get("/users", async (req, res) => {
+    try {
+        const users = await UserModel.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
-app.post("./users", async (req, res) => {
+// Método Post de Users
+app.post("/users", async (req, res) => {
     try {
         const user = await UserModel.create(req.body);
         res.status(201).json(user);
@@ -37,4 +37,43 @@ app.post("./users", async (req, res) => {
     };
 });
 
+// Buscador de Usuários
+app.get("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+// Atualizar Usuário
+// Método Patch: para atualização parcial de dados.
+// Método Put: para atualizar todos os campos de dados.
+app.patch("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+        res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+// Deletar Usuário
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndDelete(id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+})
+
+// A função que escuta por requisições
 app.listen(port, () => console.log(`Rodando com Express na porta ${port}!`));
